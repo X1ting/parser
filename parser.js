b1b4708a870f6b3a999c75380fc8c9a5213f8435
x1ting <vtakarlikov@gmail.com>
2015-11-13 21:45:29 +0300

    finish, need refactor

--- parser.js
+++ parser.js
@@ -1,84 +1,92 @@
 var parseString = require('xml2js').parseString;
 
-var exec = require('child_process').exec;
+var exec = require('child_process').execSync;
+var async = require('async');
 var fs = require('fs');
-exec('curl -X GET http://madrobots.ru/htcCatalogNew.xml | iconv -f cp1251 -t utf8 -- > new.txt', function(err, stdout, stderr) {
-  var rs = fs.readFileSync('./new.txt');
-  parseString(rs.toString(), function (err, result) {
-    // console.dir(result)
-    // console.log(err)
-     return result.yml_catalog.shop[0].categories[0].category.map((item) => {
-      console.log(item._)
-     //  // new Category({title: body, id: item["$"]["id"]}).save(function (err) {
-     //  //   console.log('imhere')
-     //  //   if (err) // ...
-     //  //   console.log('meow');
-     //  // });
-     });
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/parser');
+var Item = mongoose.model('Item',
+  {
+    title: String,
+    cover: String,
+    desc: String,
+    shops: [
+      {
+        name: String,
+        price: Number,
+        url: String,
+        date: {
+          type: String,
+          default: Date.now()
+        },
+
+      }
+    ],
+    vendor: String,
+    categories: Array,
+    id: Number,
+    photos: Array
   });
-});
-// var assert = require('assert');
-// var url = 'http://madrobots.ru/htcCatalogNew.xml';
-// var utf8 = require('utf8');
-// var fs = require('fs');
-// // var mongoose = require('mongoose');
-// // mongoose.connect('mongodb://localhost/parser');
-// // var Item = mongoose.model('Item',
-// //   {
-// //     title: String,
-// //     cover: String,
-// //     desc: String,
-// //     category: Array
-// //   });
+var config = [
+    { name: 'Madrobots',
+      url: 'http://madrobots.ru/htcCatalogNew.xml',
+      url_prepend: 'http://madrobots.ru/',
+      pictures_prepend_need: true
+    },
+    { name: 'Designboom',
+      url: 'http://designboom.ru/bitrix/catalog_export/yml.php',
+      url_prepend: 'http://designboom.ru/',
+      pictures_prepend_need: false
+    }
+  ]
+config.map((shop) => {
+  exec(('curl -X GET ' + shop.url + ' | iconv -f cp1251 -t utf8 -- > new.txt'), function(err, stdout, stderr) {
+    var rs = fs.readFileSync('./new.txt');
+    parseString(rs.toString(), function (err, result) {
+      var categories = {};
+      result.yml_catalog.shop[0].categories[0].category.map((item) => {
+          categories[item["$"].id] = item._
+       });
 
-// // var Category = mongoose.model('Category',
-// //   {
-// //     id: Number,
-// //     title: String
-// //   });
-// curl -X GET http://madrobots.ru/htcCatalogNew.xml | iconv -f cp1251 -t utf8 --
-
-//   //the whole response has been recieved, so we just print it out here
-// request({ url: 'http://madrobots.ru/htcCatalogNew.xml', headers: { 'Accept-Charset': 'utf-8' } }, function(err, response, body) {
-  // console.log(utf8.decode(body));
-  // console.log(body);
-
-   // convert from UTF-8 to ISO-8859-1
-   // console.log(response);
-
-    // var iconv = new Iconv('CP1251', 'UTF-8');
-    // var buffer = iconv.convert(body);
-    // console.log(buffer.toString())
-    // fs.writeFileSync('./govno.txt', body);
-  // console.log(body)
-  // body = new Buffer(body, 'binary');
-  // conv = new Iconv('windows-1251', 'utf8');
-  // body = conv.convert(body).toString();
-      // var iconv = new Iconv('UTF-8', 'ASCII');
-   // iconv.convert(body.toString())
-  // parseString(body, function (err, result) {
-  //    return result.yml_catalog.shop[0].categories[0].category.map((item) => {
-  //     // new Category({title: body, id: item["$"]["id"]}).save(function (err) {
-  //       // console.log('imhere')
-  //       // if (err) // ...
-  //       // console.log('meow');
-  //     // });
-  //    });
-  // });
-// });
-
-
-// // mongoose.disconnect();
-
-// var axios = require('axios');
-
-// axios.get(url)
-// .then(function(response){
-//   // var buf = iconv.encode(response.data, 'utf8');
-//   // var str = iconv.decode(buf, 'utf8');
-//   // console.log(buf.toString());
-//   // console.log(iconv.decode(new Buffer(response.data), 'utf8'));
-// })
-// .catch(function(error){
-//   console.log(error);
-// });
+      result.yml_catalog.shop[0].offers[0].offer.map((offer) => {
+        // console.log(offer)
+        Item.findOne({title: offer.model}, function (err, item) {
+          if (err)
+            console.log('find', err);
+          if (!item) {
+            var category_array = []
+            offer.categoryId.map((id) => {
+              category_array.push(categories[id])
+            })
+            var pictures = []
+            if ((offer.param[2] || {})._){
+              offer.param[2]._.split(',').map((picture) => {
+                if (shop.pictures_prepend_need)
+                  pictures.push(shop.url_prepend.concat(picture))
+                else
+                  pictures.push(picture)
+            })}
+            pictures.push((offer.picture || {})[0])
+            new Item({
+              title: offer.model[0],
+              id: offer['$'].id, 
+              shops: [{ name: shop.name, 
+                        price: offer.price[0], 
+                        url: offer.url[0]
+                      }],
+              cover: (offer.picture || {}), 
+              vendor: offer.vendor[0], 
+              desc: offer.description[0],
+              categories: category_array,
+              photos: pictures
+            }).save(function (err) {
+              if (err)
+                console.log('saveitem', err)
+            })
+          }
+        })
+      })
+    });
+  // mongoose.disconnect();
+  });
+})
