0e905873bd9237039341000ce39ce7cbe7fa85d3
x1ting <vtakarlikov@gmail.com>
2015-11-14 19:38:25 +0300

    add cron

--- parser.js
+++ parser.js
@@ -1,10 +1,24 @@
 var parseString = require('xml2js').parseString;
-
 var exec = require('child_process').exec;
 var async = require('async');
 var fs = require('fs');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/parser');
+var CronJob = require('cron').CronJob;
+var config = [
+    { name: 'Madrobots',
+      url: 'http://madrobots.ru/htcCatalogNew.xml',
+      url_prepend: 'http://madrobots.ru/',
+      pictures_prepend_need: true,
+      filename: 'mrob.txt'
+    },
+    { name: 'Designboom',
+      url: 'http://designboom.ru/bitrix/catalog_export/yml.php',
+      url_prepend: 'http://designboom.ru/',
+      pictures_prepend_need: false,
+      filename: 'dboom.txt'
+    }
+  ]
 var Item = mongoose.model('Item',
   {
     title: String,
@@ -27,72 +41,68 @@ var Item = mongoose.model('Item',
     id: Number,
     photos: Array
   });
-var config = [
-    { name: 'Madrobots',
-      url: 'http://madrobots.ru/htcCatalogNew.xml',
-      url_prepend: 'http://madrobots.ru/',
-      pictures_prepend_need: true,
-      filename: 'mrob.txt'
-    },
-    { name: 'Designboom',
-      url: 'http://designboom.ru/bitrix/catalog_export/yml.php',
-      url_prepend: 'http://designboom.ru/',
-      pictures_prepend_need: false,
-      filename: 'dboom.txt'
-    }
-  ]
-async.series({ one: function(callback) {config.map((shop) => {
-  exec(('curl -X GET ' + shop.url + ' | iconv -f cp1251 -t utf8 -- > ' +  shop.filename), function(err, stdout, stderr) {
-    // console.log(stdout)
-    var rs = fs.readFileSync(('./' + shop.filename));
-    parseString(rs.toString(), function (err, result) {
-      var categories = {};
-      result.yml_catalog.shop[0].categories[0].category.map((item) => {
-          categories[item["$"].id] = item._
-       });
-      console.log(shop)
-      result.yml_catalog.shop[0].offers[0].offer.map((offer) => {
-        // console.log(offer)
-        Item.findOne({title: offer.model}, function (err, item) {
-          if (err)
-            console.log('find', err);
-          if (!item) {
-            var category_array = []
-            offer.categoryId.map((id) => {
-              category_array.push(categories[id])
-            })
-            var pictures = []
-            if ((offer.param[2] || {})._){
-              offer.param[2]._.split(',').map((picture) => {
-                if (shop.pictures_prepend_need)
-                  pictures.push(shop.url_prepend.concat(picture))
-                else
-                  pictures.push(picture)
-            })}
-            pictures.push((offer.picture || {})[0])
-            new Item({
-              title: offer.model[0],
-              id: offer['$'].id, 
-              shops: [{ name: shop.name, 
-                        price: offer.price[0], 
-                        url: offer.url[0]
-                      }],
-              cover: (offer.picture || {}), 
-              vendor: offer.vendor[0], 
-              desc: offer.description[0],
-              categories: category_array,
-              photos: pictures
-            }).save(function (err) {
-              if (err){
-                // console.log('saveitem', err)
-              }
-            })
-          }
+
+var job = new CronJob('20 * * * * *', function() {
+  config.map((shop) => {
+    exec(('curl -X GET ' + shop.url + ' | iconv -f cp1251 -t utf8 -- > ' +  shop.filename), function(err, stdout, stderr) {
+      // console.log(stdout)
+      var rs = fs.readFileSync(('./' + shop.filename));
+      parseString(rs.toString(), function (err, result) {
+        var categories = {};
+        result.yml_catalog.shop[0].categories[0].category.map((item) => {
+            categories[item["$"].id] = item._
+         });
+        console.log(shop)
+        result.yml_catalog.shop[0].offers[0].offer.map((offer) => {
+          // console.log(offer)
+          Item.findOne({title: offer.model}, function (err, item) {
+            if (err)
+              console.log('find', err);
+            if (!item) {
+              var category_array = []
+              offer.categoryId.map((id) => {
+                category_array.push(categories[id])
+              })
+              var pictures = []
+              if ((offer.param[2] || {})._){
+                offer.param[2]._.split(',').map((picture) => {
+                  if (shop.pictures_prepend_need)
+                    pictures.push(shop.url_prepend.concat(picture))
+                  else
+                    pictures.push(picture)
+              })}
+              pictures.push((offer.picture || {})[0])
+              new Item({
+                title: offer.model[0],
+                id: offer['$'].id, 
+                shops: [{ name: shop.name, 
+                          price: offer.price[0], 
+                          url: offer.url[0]
+                        }],
+                cover: (offer.picture || {}), 
+                vendor: offer.vendor[0], 
+                desc: offer.description[0],
+                categories: category_array,
+                photos: pictures
+              }).save(function (err) {
+                if (err){
+                  // console.log('saveitem', err)
+                }
+              })
+            }
+          })
         })
-      })
+      });
     });
-  // mongoose.disconnect();
   });
-})}}, function(err, res){
-})
-setTimeout(function(){mongoose.disconnect()},100000)
+  /*
+   * Runs every weekday (Monday through Friday)
+   * at 11:30:00 AM. It does not run on Saturday
+   * or Sunday.
+   */
+  }, function () {
+    // mongoose.disconnect();
+  },
+  false, /* Start the job right now */
+  'America/Los_Angeles' /* Time zone of this job. */
+);
