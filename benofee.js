717afa32f22e551879b95475461ea820debd0453
Michael Egorov <michaelegorov@MacBook-Pro-Michael.local>
2015-12-01 18:21:53 +0300

    name concat

--- benofee.js
+++ benofee.js
@@ -95,7 +95,7 @@ var job = new CronJob('20 * * * * *', function() {
   config.map(function(shop) {
     // console.log(shop)
     exec(('curl -X GET ' + shop.url + ' | iconv -f cp1251 -t utf8 -- > ' +  shop.filename), function(err, stdout, stderr) {
-      var rs = fs.readFileSync(('./' + shop.filename));
+      var rs = fs.readFileSync(('./' + 'svt.txt'));
       // console.log(err, stdout, stderr)
       // console.log(rs.toString())
       xml2js_parseString(rs.toString(), function (err, result) {
@@ -115,11 +115,11 @@ var job = new CronJob('20 * * * * *', function() {
                 category_array.push(categories[id])
               })
               var itemTitle;
-              if(offer.vendor[0] != 'Apple')
+              if(offer.model[0].indexOf('iPad') == -1 && offer.model[0].indexOf('LG') == -1 )
                 itemTitle = offer.vendor[0] +' '+offer.model[0];
               else
                 itemTitle = offer.model[0];
-              var newItem = new Item({
+              var newItem = {
                 title: itemTitle,
                 index: offer['$'].id,
                 price: offer.price[0],
@@ -131,7 +131,7 @@ var job = new CronJob('20 * * * * *', function() {
                 vendor: offer.vendor[0],
                 desc: offer.description[0],
                 categories: category_array,
-              })
+              };
               //TODO: parse photos from site. Use cheerio
               //TODO: parse specs from site.
               //TODO:
@@ -139,7 +139,7 @@ var job = new CronJob('20 * * * * *', function() {
                 newItem.prg10 = true;
               }
 
-              Item.findOneAndUpdate({title: offer.model[0]}, newItem, {upsert: true, new: true}, function (err, item) {
+              Item.findOneAndUpdate({title: itemTitle}, newItem, {upsert: true, new: true}, function (err, item) {
                 if (err)
                   console.log('find', err);
                 // if (!item) {
