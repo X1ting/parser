e01ed945d0ff3bce52deae624331caf83afc7f00
oskolog@gmail.com <egorov@iMac-Ios.local>
2015-12-02 15:45:05 +0300

    eldorado form excel

---
+++ eldorado.js
@@ -0,0 +1,251 @@
+var xml2js_parseString = require('xml2js').parseString;
+var exec = require('child_process').exec;
+var getPictures = require('./html_parser.js').getPictures;
+var fs = require('fs');
+var mongoose = require('mongoose');
+var xlsx = require('node-xlsx');
+mongoose.connect('mongodb://localhost/benofee');
+var CronJob = require('cron').CronJob;
+var config =
+    [
+      { name: 'Эльдорадо',
+        url: 'http://www.eldorado.ru/_export/new_yandex/showprice.php?id=33',
+        url_prepend: 'http://www.eldorado.ru/',
+        pictures_prepend_need: false,
+        filename: 'eldorado.txt'
+      }
+    ];
+
+var Item = mongoose.model('Item',
+  {
+    title: String,
+    promo: {
+      type: Boolean,
+      default: false
+    },
+    price: Number,
+    desc: String,
+    specs: [{
+        name: String,
+        value: String
+    }],
+    photos: [String],
+    recommended: [String],
+    shop: String,
+    count: Number,
+    programs: Array,
+    categories: [String],
+    category: String,
+    tags: [String],
+    hide: {
+        type: Boolean,
+        default: false
+    },
+    shopname: String,
+    ordered: {
+      type: Number,
+      default: 0
+    },
+    hide: {
+      type: Boolean,
+      default: false
+    },
+    discount: {
+      type: Number,
+      default: 0
+    },
+    action_ends: Date,
+    updated: {
+      type: Date,
+      default: Date.now
+    },
+    popularity: {
+      type: Number,
+      default: 0
+    },
+    prg6: {
+      type: Boolean,
+      default: false
+    },
+    prg10: {
+      type: Boolean,
+      default: false
+    },
+    prg12: {
+      type: Boolean,
+      default: false
+    },
+    prg24: {
+      type: Boolean,
+      default: false
+    },
+    prg36: {
+      type: Boolean,
+      default: false
+    }
+});
+
+var job = new CronJob('20 1 * * * *', function() {
+  config.map(function(shop) {
+    var obj = xlsx.parse(__dirname + '/r.xlsx');
+    var hire10 = [];
+    var hire12 = [];
+    var hire24 = [];
+
+    obj[0].data.slice(6,obj[0].data.length).forEach(function(o){
+      hire10.push(o[3])
+    })
+
+    obj[1].data.slice(6,obj[1].data.length).forEach(function(o){
+      hire12.push(o[3])
+    })
+
+    obj[2].data.slice(6,obj[2].data.length).forEach(function(o){
+      hire24.push(o[3])
+    })
+    exec(('curl -X GET ' + shop.url + ' | iconv -f cp1251 -t utf8 -- > ' +  shop.filename), function(err, stdout, stderr) {
+      var rs = fs.readFileSync(('./' + shop.filename));
+      // console.log(err, stdout, stderr)
+      // console.log(rs.toString())
+      xml2js_parseString(rs.toString(), function (err, result) {
+        var categories = {};
+        // console.log(result)
+        result.yml_catalog.shop[0].categories[0].category.map(function(item) {
+            categories[item["$"].id] = item._
+         });
+        console.log(result.yml_catalog.shop[0].offers[0].offer[0]);
+        var hire10offers = [];
+        var hire12offers = [];
+        var hire24offers = [];
+
+        result.yml_catalog.shop[0].offers[0].offer.map(function(offer) {
+          // console.log(offer['$'].ids)
+          if(hire10.indexOf(parseInt(offer['$'].id)) != -1){
+            console.log('hire10 ',offer['$'].id)
+            getPictures(shop.name, offer.url[0], function(err, result){
+              console.log('hire10 ',offer['$'].id)
+              if (result) {
+                photos = result.images;
+                specs = result.specs;
+                var category_array = []
+                offer.categoryId.map(function(id) {
+                  category_array.push(categories[id])
+                })
+                var itemTitle;
+                if(offer.model[0].indexOf('iPad') == -1 && offer.model[0].indexOf('LG') == -1 )
+                  itemTitle = offer.vendor[0] +' '+offer.model[0];
+                else
+                  itemTitle = offer.model[0];
+                var newItem = {
+                  title: itemTitle,
+                  index: offer['$'].id,
+                  price: offer.price[0],
+                  shop: offer.url[0],
+                  photos: photos,
+                  specs: specs,
+                  shopname: shop.name,
+                  cover: (offer.picture || {}),
+                  vendor: offer.vendor[0],
+                  desc: offer.description[0],
+                  categories: category_array,
+                  category: category_array[0],
+                  prg10: true
+                };
+                Item.findOneAndUpdate({title: itemTitle}, newItem, {upsert: true, new: true}, function (err, item) {
+                  if (err)
+                    console.log('find', err);
+                });
+              }
+            })
+          }
+          else if(hire12.indexOf(parseInt(offer['$'].id)) != -1){
+            console.log('hire12 ',offer['$'].id)
+            getPictures(shop.name, offer.url[0], function(err, result){
+              console.log('hire12 ',offer['$'].id)
+              if (result) {
+                photos = result.images;
+                specs = result.specs;
+                var category_array = []
+                offer.categoryId.map(function(id) {
+                  category_array.push(categories[id])
+                })
+                var itemTitle;
+                if(offer.model[0].indexOf('iPad') == -1 && offer.model[0].indexOf('LG') == -1 )
+                  itemTitle = offer.vendor[0] +' '+offer.model[0];
+                else
+                  itemTitle = offer.model[0];
+                var newItem = {
+                  title: itemTitle,
+                  index: offer['$'].id,
+                  price: offer.price[0],
+                  shop: offer.url[0],
+                  photos: photos,
+                  specs: specs,
+                  shopname: shop.name,
+                  cover: (offer.picture || {}),
+                  vendor: offer.vendor[0],
+                  desc: offer.description[0],
+                  categories: category_array,
+                  category: category_array[0],
+                  prg12: true
+                };
+                Item.findOneAndUpdate({title: itemTitle}, newItem, {upsert: true, new: true}, function (err, item) {
+                  if (err)
+                    console.log('find', err);
+                });
+              }
+            })
+          }
+          else if(hire24.indexOf(parseInt(offer['$'].id)) != -1){
+            console.log('hire24 ',offer['$'].id)
+            getPictures(shop.name, offer.url[0], function(err, result){
+              console.log('hire24 ',offer['$'].id)
+              if (result) {
+                photos = result.images;
+                specs = result.specs;
+                var category_array = []
+                offer.categoryId.map(function(id) {
+                  category_array.push(categories[id])
+                })
+                var itemTitle;
+                if(offer.model[0].indexOf('iPad') == -1 && offer.model[0].indexOf('LG') == -1 )
+                  itemTitle = offer.vendor[0] +' '+offer.model[0];
+                else
+                  itemTitle = offer.model[0];
+                var newItem = {
+                  title: itemTitle,
+                  index: offer['$'].id,
+                  price: offer.price[0],
+                  shop: offer.url[0],
+                  photos: photos,
+                  specs: specs,
+                  shopname: shop.name,
+                  cover: (offer.picture || {}),
+                  vendor: offer.vendor[0],
+                  desc: offer.description[0],
+                  categories: category_array,
+                  category: category_array[0],
+                  prg24: true
+                };
+                Item.findOneAndUpdate({title: itemTitle}, newItem, {upsert: true, new: true}, function (err, item) {
+                  if (err)
+                    console.log('find', err);
+                });
+              }
+            })
+          }
+        })
+      });
+    });
+  });
+  /*
+   * Runs every weekday (Monday through Friday)
+   * at 11:30:00 AM. It does not run on Saturday
+   * or Sunday.
+   */
+  }, function () {
+    mongoose.disconnect();
+  },
+  true, /* Start the job right now */
+  'America/Los_Angeles' /* Time zone of this job. */
+);
